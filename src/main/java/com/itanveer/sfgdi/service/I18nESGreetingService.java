9a3f3093d565335e0913566836e230f4e7c9c48c
iTanveerAhmad <talktanveerkhan@outlook.com>
2021-06-18 08:42:04 +0200

    add default in profile using @Profile()

--- src/main/java/com/itanveer/sfgdi/service/I18nESGreetingService.java
+++ src/main/java/com/itanveer/sfgdi/service/I18nESGreetingService.java
@@ -3,7 +3,7 @@ package com.itanveer.sfgdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nESGreetingService implements GreetingService{
 
